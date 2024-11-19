742d464ac68dc3a686dd44cd2530ab035a8c071d
quentinpiot <qkpiot@gmail.com>
2024-11-19 18:11:27 +0530

    WIP: add api homepage

--- apps/backend/api-gateway/src/app.controller.ts
+++ apps/backend/api-gateway/src/app.controller.ts
@@ -22,4 +22,71 @@ export class AppController {
         }),
     ]);
   }
+
+
+  @Get()
+  getHome(): string {
+    return `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta name="description" content="Quentin Piot personals projects apis and documentations">
+          <meta name="author" content="Quentin Piot">
+          <meta name="viewport" content="width=device-width, initial-">
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+          <title>API - Quentin Piot</title>
+          <style>
+            body {
+              background-image: linear-gradient(120deg, #a6c0fe 0%, #f68084 100%);
+              color: white;
+              font-family: 'Roboto', sans-serif;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              height: 100vh;
+              margin: 0;
+              text-align: center;
+            }
+            h1 {
+              font-size: 4rem;
+              margin-bottom: 20px;
+            }
+            p {
+              font-size: 1.2rem;
+            }
+            a {
+              color: white;
+              text-decoration: none;
+              transition: color 0.3s ease;
+            }
+            a:hover {
+              color: #ff8084;
+            }
+            .container {
+              padding: 40px;
+              background: rgba(255, 255, 255, 0.2);
+              border-radius: 16px;
+              box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1); 
+              backdrop-filter: blur(5px);
+              -webkit-backdrop-filter: blur(5px);
+              border: 1px solid rgba(255, 255, 255, 0.3);
+            }
+            .welcome {
+              font-weight: bold;
+              color: #ffffff;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <h2 class="welcome">⚠️ Building in progress ⚠️</h2>
+            <p style="font-size: 0">Quentin Piot</p>
+            <a href="https://quentinpiot.com">quentinpiot.com</a>
+          </div>
+        </body>
+      </html>
+    `;
+  }
 }
