7f83eef12bd48b1c8f33b232d2894bc2208e5ecd
quentinpiot <qkpiot@gmail.com>
2024-11-20 17:49:30 +0530

    WIP: fix api gateway homepage

--- apps/backend/api-gateway/src/app.controller.ts
+++ apps/backend/api-gateway/src/app.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from "@nestjs/common";
-import { HealthCheck, HealthCheckService, GRPCHealthIndicator } from "@nestjs/terminus";
+import { GRPCHealthIndicator, HealthCheck, HealthCheckService } from "@nestjs/terminus";
 import { join } from "path";
 
 @Controller("")
@@ -7,14 +7,15 @@ export class AppController {
   constructor(
     private health: HealthCheckService,
     private grpc: GRPCHealthIndicator,
-  ) {}
+  ) {
+  }
 
   @Get("/health")
   @HealthCheck()
   async check() {
     return this.health.check([
       async () =>
-        this.grpc.checkService("user_service","user_service", {
+        this.grpc.checkService("user_service", "user_service", {
           timeout: 500,
           package: "user",
           protoPath: join(__dirname, "..", "node_modules", "@quentinpiot", "protos", "user.proto"),
@@ -39,13 +40,14 @@ export class AppController {
           <title>API - Quentin Piot</title>
           <style>
             body {
-              background-image: linear-gradient(120deg, #a6c0fe 0%, #f68084 100%);
+              background-color: black;
               color: white;
               font-family: 'Roboto', sans-serif;
               display: flex;
               justify-content: center;
               align-items: center;
               height: 100vh;
+              overflow: hidden;
               margin: 0;
               text-align: center;
             }
@@ -77,13 +79,49 @@ export class AppController {
               font-weight: bold;
               color: #ffffff;
             }
+            @keyframes rotate {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.gradient {
+  position: absolute;
+  left: calc(50% - 25vw);
+  top: calc(50% - 25vw);
+  width: 50vw;
+  height: 50vw;
+  filter: blur(calc(750px / 5));
+  background-image:linear-gradient(120deg, #a6c0fe 0%, #f68084 100%);
+  animation: rotate 10s cubic-bezier(0.8, 0.2, 0.2, 0.8) alternate infinite;
+  border-radius: 30% 70% 70% 30% / 30% 30% 70% 70%;
+}
+
+@media (max-width: 1000px) {
+  .gradient {
+
+  left: calc(50% - 25vh);
+  top: calc(50% - 25vh);
+  width: 50vh;
+  height: 50vh;  }
+}
+
+
+/* This is just to transition when you change the viewport size. */
+* {
+  transition: all 0.5s ease-out;
+}
+
           </style>
         </head>
         <body>
+          <div class="gradient"></div>
           <div class="container">
-            <h2 class="welcome">⚠️ Building in progress ⚠️</h2>
+            <h2 class="welcome">Building in progress</h2>
             <p style="font-size: 0">Quentin Piot</p>
-            <a href="https://quentinpiot.com">quentinpiot.com</a>
           </div>
         </body>
       </html>
